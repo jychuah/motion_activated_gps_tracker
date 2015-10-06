33e28b3235bc3e09a0b65c31b841e8882fe423a8
jychuah <jychuah@gmail.com>
2015-10-05 19:02:14 -0500

    Sequence posting now works
    Successfully posts the following events:
    New Sequence
    GPS Location

--- Adafruit_FONA_Test/Adafruit_FONA_Test.ino
+++ Adafruit_FONA_Test/Adafruit_FONA_Test.ino
@@ -1,36 +1,19 @@
 /***************************************************
-This is an example for our Adafruit FONA Cellular Module
+This code is based off of example code from the Adafruit FONA Cellular Module
+with credits to Limor Fried/Ladyada from Adafruit Industries.
 
-Designed specifically to work with the Adafruit FONA
+Designed specifically to work with the Adafruit FONA Sim808
 ----> http://www.adafruit.com/products/1946
 ----> http://www.adafruit.com/products/1963
 ----> http://www.adafruit.com/products/2468
 ----> http://www.adafruit.com/products/2542
 
-These cellular modules use TTL Serial to communicate, 2 pins are
-required to interface
-Adafruit invests time and resources providing this open source code,
-please support Adafruit and open-source hardware by purchasing
-products from Adafruit!
+Please support Adafruit.
 
-Written by Limor Fried/Ladyada for Adafruit Industries.
 BSD license, all text above must be included in any redistribution
 ****************************************************/
 
-/*
-THIS CODE IS STILL IN PROGRESS!
-
-Open up the serial console on the Arduino at 115200 baud to interact with FONA
-
-Note that if you need to set a GPRS APN, username, and password scroll down to
-the commented section below at the end of the setup() function.
-*/
 #include "Adafruit_FONA.h"
-
-#define APN			"fast.t-mobile.com"
-#define UID			"d76db2b8-be35-477c-a428-2623d523fbfd"
-#define IMEI		"865067020757418"
-#define HELPER_URL	"http://webpersistent.com/motorbike-tracker/helper/post.php"
 
 #define DEBUG true
 
@@ -43,23 +26,33 @@ the commented section below at the end of the setup() function.
 #define EVENT_INDEX 89
 #define DATA_INDEX 118
 // Sequence IDs are 20 characters, +4 padding characters
-#define SEQUENCE_INDEX 216
+#define SEQUENCE_INDEX 255
 
-#define setEvent(a)	strncpy(&postdata[EVENT_INDEX], a, strlen(a));
+
+#define HELPER_URL  "http://webpersistent.com/motorbike-tracker/helper/post.php"
+#define setEvent(a)		strncpy_P(&postdata[EVENT_INDEX], a, strlen(a));
+#define postError()		strncmp_P(postresult, ERROR, strlen(ERROR)) == 0
+
+/********************************
+ User specific settings
+*********************************/
+#define		APN					"fast.t-mobile.com"
+#define     UID					"d76db2b8-be35-477c-a428-2623d523fbfd"
+#define		IMEI				"865067020757418"
 
 // Event strings
 const char BATTERY[] PROGMEM = "Battery";
 const char NEW_SEQUENCE[] PROGMEM = "NEW SEQUENCE";
 const char GPS_LOCATION[] PROGMEM = "GPS Location";
+const char ERROR[] PROGMEM = "ERROR";
 
 char postdata[] = "{ \"uid\" : \"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx\", "
 	"\"imei\" : \"xxxxxxxxxxxxxxx\", \"event\" : \"event_type_string\","
-	"\"data\" : \"01234567891123456789212345678931234567894123456789512345678961234567897123456789"
-	"8123456789912345678901234567891123456789\", \"sequence\" : \"xxxxxxxxxxxxxxxxxxxx    \" }";
-char postresult[30];
+	"\"data\" : \"012345678911234567892123456789312345678941234567895123456789612345678971234567898123456789912345678901234567891123456789\""
+	", \"sequence\" : \"                        \" }";
 
-// this is a large buffer for replies
-char replybuffer[255];
+#define POST_RESULT_LENGTH 30
+char postresult[POST_RESULT_LENGTH];
 
 // This is to handle the absence of software serial on platforms
 // like the Arduino Due. Modify this code if you are using different
@@ -75,8 +68,6 @@ HardwareSerial *fonaSerial = &Serial1;
 
 // Use this for FONA 800 and 808s
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
-// Use this one for FONA 3G
-//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
 
 uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
 
@@ -90,6 +81,7 @@ void setup() {
 	Serial.println(F("Initializing....(May take 3 seconds)"));
 
 	fonaSerial->begin(4800);
+	
 	if (!fona.begin(*fonaSerial)) {
 		Serial.println(F("Couldn't find FONA"));
 		while (1);
@@ -105,63 +97,55 @@ void setup() {
 
 	// Set UID in post data
 	strncpy(&postdata[UID_INDEX], UID, strlen(UID));
+	clearPostData();
 
-	initPostData();
-
-#ifdef DEBUG
-	Serial.print("Post data: ");
-	Serial.println(postdata);
-#endif
-	// Optionally configure a GPRS APN, username, and password.
-	// You might need to do this to access your network's GPRS/data
-	// network.  Contact your provider for the exact APN, username,
-	// and password values.  Username and password are optional and
-	// can be removed, but APN is required.
 	fona.setGPRSNetworkSettings(F(APN));
 
-	// Optionally configure HTTP gets to follow redirects over SSL.
-	// Default is not to follow SSL redirects, however if you uncomment
-	// the following line then redirects over SSL will be followed.
-	//fona.setHTTPSRedirect(true);
-
-#ifdef DEBUG
 	Serial.println(F("Enabling GPRS"));
-#endif
-	while(!fona.enableGPRS(true));
+	while(!fona.enableGPRS(true)) delay(5000);
 
-#ifdef DEBUG
 	Serial.println(F("Enabling GPS"));
-#endif
-	while(!fona.enableGPS(true));
+	while(!fona.enableGPS(true)) delay(5000);
 
-// Test sequence
-#ifdef DEBUG
 	Serial.println(F("Starting sequence"));
-	while (!newSequence());
-#endif
+	while (!newSequence()) delay(5000);
+}
+
+void loop() {
+	// flush input
+	flushSerial();
+	while (fona.available()) {
+		Serial.write(fona.read());
+	}
+
+	logGPSLocation();
+	delay(10000);
+
 }
 
 void initPostData() {
+	strncpy(&postdata[UID_INDEX], UID, strlen(UID));
+	strncpy(&postdata[IMEI_INDEX], IMEI, strlen(IMEI));
+}
+
+void clearPostData() {
 	memset(&postdata[EVENT_INDEX], ' ', 17);
 	memset(&postdata[DATA_INDEX], ' ', 120);
-	memset(&postdata[SEQUENCE_INDEX], ' ', 24);
 }
 
 bool sendPostData() {
 	uint16_t statuscode;
 	int16_t length;
 
-	if (!fona.HTTP_POST_start(HELPER_URL, F("text/plain"), (uint8_t *)postdata, strlen(postdata), &statuscode, (uint16_t *)&length)) {
-#ifdef DEBUG
+	if (!fona.HTTP_POST_start(HELPER_URL, F("application/json"), (uint8_t *)postdata, strlen(postdata), &statuscode, (uint16_t *)&length)) {
 		Serial.println("Failed!");
-#endif
 		return false;
 	}
-
 	int i = 0;
 	while (length > 0) {
 		while (fona.available()) {
 			char c = fona.read();
+
 			postresult[i] = c;
 
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
@@ -173,18 +157,12 @@ bool sendPostData() {
 
 			length--;
 			i++;
-			if (!length || i >= 30) break;
+			if (!length || i >= POST_RESULT_LENGTH) break;
 		}
 	}
-	memset(&postresult[i], 0, 30 - i);
+	memset(&postresult[i], 0, POST_RESULT_LENGTH - i);
 	Serial.println(F("\n"));
 	fona.HTTP_POST_end();
-
-#ifdef DEBUG
-	Serial.print(F("Post result: "));
-	Serial.println(postresult);
-#endif
-
 	return true;
 }
 
@@ -193,58 +171,38 @@ void checkBattery() {
 
 	if (fona.getBattPercent(&vbat)) {
 		if (vbat < 10) {
-			initPostData();
+			clearPostData();
 			setEvent(BATTERY);
+
 			// TODO: copy battery to data...
 
 			sendPostData();
 		} 
-	}
-#ifdef DEBUG
-	else {
+	} else {
 		Serial.print(F("Failed to read battery"));
 	}
-#endif	
 }
 
-void logGPSLocation() {
-	initPostData();
+bool logGPSLocation() {
+	clearPostData();
 	setEvent(GPS_LOCATION);
 	fona.getGPS(0, &postdata[DATA_INDEX], 120);
 	sendPostData();
-#ifdef DEBUG
-	Serial.print(F("GPS Data: "));
-	Serial.println(postdata[DATA_INDEX], 120);
-#endif
+	if (postError()) {
+		return false;
+	}
+	return true;
 }
 
 bool newSequence() {
-	//TODO: Test
-	initPostData();
+	clearPostData();
 	setEvent(NEW_SEQUENCE);
 	sendPostData();
-	if (strlen(postresult) == 20) {
-		strncpy(&postdata[SEQUENCE_INDEX], postresult, strlen(postresult));
-		return true;
-	}
-	else {
+	if (postError()) {
 		return false;
 	}
-}
-
-void loop() {
-	Serial.print(F("FONA> "));
-	while (!Serial.available()) {
-		if (fona.available()) {
-			Serial.write(fona.read());
-		}
-	}
-	// flush input
-	flushSerial();
-	while (fona.available()) {
-		Serial.write(fona.read());
-	}
-
+	strncpy(&postdata[SEQUENCE_INDEX], postresult, strlen(postresult));
+	return true;
 }
 
 void flushSerial() {
